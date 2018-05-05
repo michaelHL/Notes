6eac0c3d29303de3b7c183660684d125a2b266c7
Shine Mic <lbgnmic@gmail.com>
2018-05-05 20:10:34 +0800

    update

--- pull.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-cd "$(dirname "$0")"
-echo -e "\033[31;49;1m>>>>>>> PULLING <<<<<<< \033[39;49;0m"
-git fetch --all
-git reset --hard origin/master
-git pull
-echo -e "\033[31;49;1m>>>>>>>   DONE  <<<<<<< \033[39;49;0m"
